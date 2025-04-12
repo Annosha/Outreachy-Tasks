abc776fe80a14960ae1cea9bcd002db3c6f01c16
annosha <annosha001@gmail.com>
2025-04-12 15:33:21 +0500

    Readme tweaks

--- Task 2/asyncio_code.py
+++ Task 2/asyncio_code.py
@@ -5,7 +5,7 @@ import aiohttp  # Asynchronous HTTP client
 from urllib.parse import urlparse   # To validate URL format
 
 # --- Configuration ---
-CSV_FILE = "Task 2/Task2_Intern.csv"  # Input CSV file containing URLs 
+CSV_FILE = "Task 2/input_urls.csv"  # Input CSV file containing URLs 
 CONCURRENCY_LIMIT = 10            # Max number of concurrent requests
 RETRY_STATUS_CODES = {429, 503}   # Retry for these HTTP status codes (rate limit or unavailable)
 MAX_RETRIES = 3                   # Retries to handle temporary issues like rate limiting, timeouts, or service unavailability.
--- Task 2/multithreading_code.py
+++ Task 2/multithreading_code.py
@@ -78,7 +78,7 @@ def get_status_codes(input_csv, output_csv, max_threads=10):
 
 
 # --- Run the script ---
-input_csv_path = "Task 2/Task2_Intern.csv"
+input_csv_path = "Task 2/input_urls.csv"
 output_csv_path = "Task 2/output_multithreading.csv"
 
 get_status_codes(input_csv_path, output_csv_path)
